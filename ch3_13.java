868c7851581d781bff0878aa64813d2d07f802e2
y56 <36190560+y56@users.noreply.github.com>
2019-05-02 16:31:31 +0800

    Update ch3_13.java

--- ch3_13.java
+++ ch3_13.java
@@ -4,7 +4,8 @@ public class Main {
         Static methods can be called without creating an instance of a class.
 
         For example, the sqrt() method of standard Math class is static.
-        Hence, we can directly call Math.sqrt() without creating an instance of Math class.                                            */
+        Hence, we can directly call Math.sqrt() without creating an instance of Math class.
+*/
     public static void showMatrix(int M[][], int n) {
         for (int i = 0; i <= n-1; i++) {
             for (int j = 0; j <= n-1; j++) {
@@ -13,20 +14,36 @@ public class Main {
         System.out.println();
         }
     }
-    public static int minmult(int n, int[] d) {
+    public static void order(int[][] myP, int i, int j) {
+        if (i == j) {
+            System.out.printf("A%d", i+1); // +1 is to fit the indexing of the exercise
+        }
+        else{
+            int k = myP[i][j];
+            System.out.printf("(");
+            order(myP, i,k);
+            order(myP, k+1,j);
+            System.out.printf(")");
+
+        }
+    }
+    public static int minMult(int n, int[] d) {
 
         int[][] M = new int[n][n];  // Set the M matrix.
-        int[][] P = new int[n][n];  // Set the P matrix.
+        int[][] myP = new int[n][n];  // Set the myP matrix.
+        int[][] P = new int[n][n];  // Set the P matrix for answering the exercise.
         for (int i = 0; i <= n-1; i++) {
             for (int j = 0; j <= n-1; j++) {
             M[i][j] = 0;
+            myP[i][j] = 0;
             P[i][j] = 0;
             }
         }
         System.out.println("M[][] =");
         showMatrix(M, n);
 
-        for (int diagonalIndex = 1; diagonalIndex <= n-1; diagonalIndex++) {  // n matrices need n-1 diagonals to solve the instance
+        for (int diagonalIndex = 1; diagonalIndex <= n-1; diagonalIndex++) {  
+            // n matrices need n-1 diagonals to solve the instance
             // We index the longest/middle diagonal as 0^th
             // The first diagonal we want to deal is 1^th
             // The minimum-multiplication number is the (n-1)^th diagonal, which only has one number in it.
@@ -50,23 +67,29 @@ public class Main {
                     temp = M[i][k] + M[k+1][j] + d[i]*d[k+1]*d[j+1];
                     
                     if (temp < currentMin){
-                        System.out.print("smaller!  "); System.out.print(temp); System.out.print(" < "); System.out.println(currentMin);
+                        System.out.printf("smaller!      %d < %d\n", temp, currentMin);
                         currentMin = temp;
                         currentk = k;
                     }
                     else{
-                        System.out.print("NOT smaller!  "); System.out.print(temp); System.out.print(" > "); System.out.println(currentMin);
+                        System.out.printf("NOT smaller!      %d > %d\n", temp, currentMin);
                     }
                 }
                 M[i][j] = currentMin;
-                P[i][j] = currentk;
+                myP[i][j] = currentk;
+                P[i][j] = currentk + 1;
                 System.out.print("Choose k = "); System.out.println(currentk);
                 System.out.println("M[][] =");
                 showMatrix(M, n);
             }
         }
-        System.out.println("P[][] =");
+        System.out.println("P[][] by the exercise's indexing =");
         showMatrix(P, n);
+
+        System.out.println("multiplication order by the exercise's indexing =");
+        order(myP, 0, n-1);
+        System.out.printf("\n");
+
         return M[0][n-1];
     }
     public static void main(String[] args) {
@@ -79,7 +102,8 @@ public class Main {
         d[4] = 2;
         d[5] = 50;
 
-        int M = minmult(n, d);
+        int minMultNum = minMult(n, d);
+
         System.out.println("FINISH!!!");
         System.out.println("(*￣▽￣)/‧☆*\"\u0060\'*-.,_,.-*\'\u0060\"*-.,_☆");
     }
