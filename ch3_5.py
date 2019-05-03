9d48b1bd0cc94d509de763f43e59f3ac18ad1679
y56 <36190560+y56@users.noreply.github.com>
2019-05-03 21:21:08 +0800

    Update ch3_5.py

--- ch3_5.py
+++ ch3_5.py
@@ -9,28 +9,37 @@ W = [[ 0,  4, -1, -1, -1, 10, -1],
      [-1, -1, -1, -1, -1,  0, 10],
      [-1, -1, -1,  8, -1, -1,  0]]
 
+print("At the beginning, W is")
+for _ in range(len(W)): print(W[_])
+print("\n")
+
 fullIndexList = [0] * len(W)
 for k in range(len(W)):
     fullIndexList[k] = k
 
 for skip in range(0,len(W)):      # n loops
+
     indexList = fullIndexList[:]  # hard-copy
     indexList.pop(skip)           # eliminate the "skip" 
-    print(indexList)
+
+    print("Skiping", skip, ". We are working on", indexList)
+    
     for i in indexList:      # (n-1) loops
         for j in indexList:  # (n-1) loops
-            print(W[skip][j], "+",  W[i][skip], "vs",  W[i][j])
+            print("Woring on W[ %d ][ %d ];  %2d + %2d vs %2d" %(i, j, W[skip][j], W[i][skip], W[i][j]), end=';  ')
             if W[skip][j] != -1 and W[i][skip] != -1:  # they are normal numbers
                 if W[i][j] == -1: # originally an Inf
                     W[i][j] = W[skip][j] + W[skip][j]
-                    print("choose", W[skip][j], "+", W[skip][j])
+                    print("choose W[ %d ][ %d ] =  %2d + %2d" %(i, j, W[skip][j], W[i][skip]))
                 else:  # they all are normal numbers, we have to compare  
                     if W[skip][j] + W[i][skip] < W[i][j]:    
                         W[i][j] = W[skip][j] + W[skip][j]
-                        print("choose", W[skip][j], "+", W[skip][j])
+                        print("choose W[ %d ][ %d ] =  %2d + %2d" %(i, j, W[skip][j], W[i][skip]))
                     else:
-                        print("choos", W[i][j])
+                        print("choose W[ %d ][ %d ] =  %2d" %(i, j, W[i][j]))
             else: # there are at least one Inf; impossiple for a shorter path
-                print("choose", W[i][j])
-
+                print("choose W[ %d ][ %d ] =  %2d" %(i, j, W[i][j]))
+    print("\nNow, W is")
     for _ in range(len(W)): print(W[_])
+    print("\n")
+
