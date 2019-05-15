e353fb9b415815b905c8f4139d8ca09f4ca8a762
y56 <36190560+y56@users.noreply.github.com>
2019-05-15 21:31:56 +0800

    Update ch3_39.java

--- ch3_39.java
+++ ch3_39.java
@@ -1,7 +1,8 @@
 public class LongestCommonSubstring {
     
+    // draw a chart for dynamic programming
     public static void printChart(int[][] A, char[] S1, char[] S2) {
-        
+    
         System.out.print("     ");
         for (int j = 0; j < S2.length; j++){
             System.out.print(j + "    ");
@@ -24,63 +25,49 @@ public class LongestCommonSubstring {
         }
         System.out.println();
     }
-        
-    public static void printArray(int[] array) {
-        
-        for (int i = 0; i < array.length; i++){
-            System.out.print(array[i]+"  ");
-        }System.out.println();}
-    
-    public static void plusOneOnRightDownBlockOf(int[][] A, int i, int j){
-        int S1_length = A.length;
-        int S2_length = A[0].length;
-        for (int ii=i;ii<S1_length;ii++ ){
-            for (int jj=j;jj<S2_length;jj++ ){
-                A[ii][jj] = A[ii][jj] + 1;
-            }}};
-    
+    // prepare a `Node` class to plant a tree
     public static class Node {
         
-        public char myChar;
+        public char myChar;  // a character contained in a node
         public Node leftChild;
         public Node rightChild;
         
         public void append(Node childNode) {
-            if  (null == leftChild) {
+            if  (null == leftChild) {  // start to add a child from the left
                 this.leftChild = childNode;
             }
-            else if (null == rightChild) {
+            else if (null == rightChild) {  // if the left is used, use the right
                 this.rightChild = childNode;
             }
             else { // left and right both already have child  
                 System.out.println("Can't have more children");
             }
         }
-        public Node() {
-            this.myChar = '\u0000';
+        public Node() {  // a constructor without input 
+            this.myChar = '\u0000';  // use null char as default
         }
-        public Node(char newChar) {
+        public Node(char newChar) { // a constructor with input
             this.myChar = newChar;
         }
     }
-            
+    // fill the DP chart, and plant a tree containing matching chars
     public static void fillChartPlantTree(int i, int j, int[][] A, char[] S1, char[] S2, Node node){
-        if (i < S1.length && j < S2.length){
+        if (i < S1.length && j < S2.length){  // if indecies are within bound
             if (S1[i] == S2[j]) { // match at the corner, go next
                 System.out.printf("(%d , %d )  ", i, j);
                 System.out.println("A hit at the corner, go to next corner");
-                A[i][j] += 1;
-                Node newNode = new Node(S1[i]);
+                A[i][j] += 1; // fill the DP chart
+                Node newNode = new Node(S1[i]);  // create a new node, using the matching char
                 node.append(newNode);
-                fillChartPlantTree(i + 1, j + 1, A, S1, S2, newNode);
+                fillChartPlantTree(i + 1, j + 1, A, S1, S2, newNode); // pass the newNode as a leaf of the tree
             }
-            else{
+            else{ // not matching at the corner
                 System.out.printf("(%d , %d )  ", i, j);
                 System.out.println("No hits at the corner. ");
                 A[i][j] += -7;
 
                 // Search downward.
-                int iGoDown = i+1; // one box downward
+                int iGoDown = i+1; // start from the element downward
                 boolean foundInColumn = false;
                 while (!foundInColumn && iGoDown < S1.length) {
                     if (S1[iGoDown] == S2[j]){
@@ -93,7 +80,7 @@ public class LongestCommonSubstring {
                         node.append(newNode);
                         fillChartPlantTree(iGoDown + 1, j + 1,A,S1,S2,newNode);
                     }
-                    else{
+                    else{  // no matching in this column
                         A[iGoDown][j] += -1;
                         System.out.printf("(%d , %d~)  ", iGoDown - 1, j);
                         System.out.println("No hit in column");
@@ -101,7 +88,7 @@ public class LongestCommonSubstring {
                     iGoDown++;
                 }
                 // Search rightward.
-                int jGoRight = j + 1; // one box rightward
+                int jGoRight = j + 1; // one element rightward
                 boolean foundInRow = false;
                 while (!foundInRow && jGoRight < S2.length) {
                     if (S1[i] == S2[jGoRight]) {
@@ -113,7 +100,7 @@ public class LongestCommonSubstring {
                         node.append(newNode);
                         fillChartPlantTree(i + 1, jGoRight + 1, A, S1, S2, newNode);
                     }
-                    else{
+                    else{  // no matching in this row
                         A[i][jGoRight] -= 1;
                         System.out.printf("(%d , %d~)  ", i, jGoRight); 
                         System.out.println("No hit in row");
@@ -129,12 +116,13 @@ public class LongestCommonSubstring {
             }
         }
         else{
-            // do nothing
+            // index out of bound, do nothing
             System.out.printf("(%d , %d )  ", i, j);
             System.out.println("i or j is out of index");
         }
     }
-    
+    // print along a tree
+    // TODO: find the longest string along the tree
     public static void printAlongNodes(Node node) {
         if (null != node) {
             if ('\u0000' != node.myChar) {
@@ -158,20 +146,22 @@ public class LongestCommonSubstring {
         fillChartPlantTree(0,0,A,S1,S2,head);
         
         System.out.println();
-        printAlongNodes(head);
+        printAlongNodes(head);  // the tree has been attached to `head`
         
         System.out.println();
         printChart(A, S1, S2);
     }
         
     public static void main(String[] args) {
-        //char[] S1 = {'A','$','C','M','A','*','M','N'}; 
+        
+        // test sample #1
         char[] S1 = {'A', 'X', 'M', 'G', 'J', 'K', 'C', '4', 'A', 'N', 'B'};
         char[] S2 = {'A', '$', 'C', '4', 'I', 'Q', 'M', 'A', '*', 'M', 'N'};
         
-        //char[] S1 = {'A','B'}; 
+        // test sample #2
+        //char[] S1 = {'A','$','C','M','A','*','M','N'};
         //char[] S2 = {'A','X','M','C','4','A','N','B'};
-        //char[] S2 = {'1','2','3','4','5'}; 
+        
         LCS(S1, S2); // choose the longer as S1
         
         System.out.println("The end.");
